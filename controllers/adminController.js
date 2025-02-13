d9ac08657dd5451f81bbcdaa693eb2543610e6a3
ArkanFarrel <arkanfarrel07@gmail.com>
2025-02-13 09:22:49 +0700

    Update terbaru

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,9 +6,10 @@ import Admin from '../model/adminModel.js';
 
 export const createAdmin = async (req, res) => {
     try {
-      const { email, password } = req.body;
+      const { name, email, password } = req.body;
   
       const admin = await Admin.create({
+        name,
         email,
         password,
       });
@@ -42,9 +43,9 @@ export const createAdmin = async (req, res) => {
   export const updateAdmin = async (req, res) => {
     try {
       const { id } = req.params;
-      const { email, password } = req.body;
+      const { name, email, password } = req.body;
       const [updated] = await Admin.update(
-        { email, password },
+        { name, email, password },
         { where: { id } }
       );
       if (updated) {
@@ -71,53 +72,3 @@ export const createAdmin = async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   };
-
-  // export const loginAdmin = async (req, res) => {
-  //   try {
-  //       const admin = await Admin.findOne({
-  //           where: {
-  //               email: req.body.email
-  //           }
-  //       })
-  //       if (admin !== null) {
-
-  //           const match = req.body.password === admin.password
-  //           console.log(match)
-  //           if (!match) {
-  //               console.log('password')
-  //               res.status(400).json("password salah")
-  //           }else {
-  //               const adminId = admin.id
-  //               const email = admin.email
-  //               const password = admin.password
-
-  //           //? payload
-  //           const accessToken = jwt.sign({ adminId, email, password }, process.env.SECRET_ACCESS_TOKEN, {
-  //               expiresIn: '1h'
-  //           })
-  //           const refreshToken = jwt.sign({ adminId, email, password }, process.env.SECRET_REFRESH_TOKEN, {
-  //               expiresIn: '1d'
-  //           })
-
-  //           //? simpan refresh token dalam database
-  //           await Admin.update({ refresh_token: refreshToken }, {
-  //               where: {
-  //                   id: adminId
-  //               }
-  //           })
-            
-  //           res.cookie('refreshToken', refreshToken, {
-  //               httpOnly: true,
-  //               maxAge: 24 * 60 * 60 * 1000,
-                
-  //           })
-            
-
-  //           res.json({ accessToken })
-  //       }
-  //       } else {
-
-  //           res.status(500).json("Email belum terdaftar")
-  //       }
-  //   } catch (err) {
-  //       res.status(500).json(err)
